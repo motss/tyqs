25d166bd97126da65888ae15f65267df107dff87
The web walker <contact@motss.app>
2023-02-20 00:08:36 +0800

    chore: standardize test cases in tests and benchmarks (#18)
    * chore: standardize test cases for tests and benchmarks
    
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- src/benchmarks/parse.bench.ts
+++ src/benchmarks/parse.bench.ts
@@ -1,110 +1,19 @@
 import { bench } from 'vitest';
 
 import { parse } from '../parse.js';
-import type { ParseOptions } from '../types.js';
+import { testCasesForParse } from '../tests/constants.js';
 
-interface BenchParams {
-  value: string;
-  options: ParseOptions;
-}
+testCasesForParse
+  .map(({ params, options }) => ({ params, options }))
+  .forEach(({
+    params,
+    options,
+  }) => {
+    const paramName1 = params === '' ? '<empty_string>' : params;
+    const paramName2 = options?.replacer && '[Function replacer]';
+    const benchName = `${paramName1}, ${paramName2}`;
 
-bench('<empty_string>', () => {
-  parse('');
-});
-
-[
-  'a=a',
-  'a=a&a=b',
-  'a.a=a',
-  'a.a=a&a.a=b',
-  'a.a=a&a.b=b',
-  'a[a]=a',
-  'a[a]=a&a[a]=b',
-  'a[a]=a&a[b]=b',
-  'a=a,b',
-  'a=a,b&a=c',
-  'a=a,b&b=b',
-  'a=a,b&b=b&a=c',
-  'a.a=a,b',
-  'a.a=a,b&a.a=c',
-  'a.a=a,b&a.a=c,d',
-  'a.a=a,b&a.b=c,d&b=b',
-  'a.a=a,b&a[b].c=c,d&a.c[b]=e',
-  'a.a=a&a=a',
-].forEach((value) => {
-  bench(value, () => {
-    parse(value);
+    bench(benchName, () => {
+      parse(params, options?.replacer);
+    });
   });
-});
-
-// optional replacer function
-([
-  {
-    options: {
-      replacer({ firstRawValue: [fv], key, value }) {
-        if (key === 'a' ) return fv;
-        return value;
-      },
-    },
-    value: 'a=a&a=b',
-  },
-  {
-    options: {
-      replacer({ firstRawValue, key, value }) {
-        if (key === 'a' ) return firstRawValue;
-        return value;
-      },
-    },
-    value: 'a=a,b&a=b',
-  },
-  {
-    options: {
-      replacer({ firstRawValue: [fv], key, value }) {
-        if (key === 'a.a') return fv;
-        return value;
-      },
-    },
-    value: 'a.a=a&a[a]=b',
-  },
-  {
-    options: {
-      replacer({ firstRawValue, key, value }) {
-        if (key === 'a') return firstRawValue.map(n => Number(n));
-        return value;
-      },
-    },
-    value: 'a=1,2,3',
-  },
-  {
-    options: {
-      replacer({ firstRawValue, key, value }) {
-        if (key === 'a') return firstRawValue.map(n => Number(n));
-        if (key === 'b') return firstRawValue.at(0) === 'true';
-        return value;
-      },
-    },
-    value: 'a=1,2,3&b=true',
-  },
-  {
-    options: {
-      replacer({ firstRawValue: [fv], key, value }) {
-        switch (key) {
-          case 'a': return Number(fv);
-          case 'b.a': return fv === 'true';
-          case 'c': return fv === '' ? undefined : fv;
-          case 'd': return fv === 'null' ? null : fv;
-          case 'e': return fv === 'undefined' ? undefined : fv;
-          default: return value;
-        }
-      },
-    },
-    value: 'a=1&b.a=true&c=&d=null&e=undefined',
-  },
-] as BenchParams[]).forEach(({
-  options,
-  value,
-}) => {
-  bench(`${value} (options: ${options})`, () => {
-    parse(value, options.replacer);
-  });
-});
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   test: {
     coverage: {
+      exclude: [
+        /**
+         * NOTE: Code coverage collects other files inside `tests` folder, e.g. `constants.ts` but
+         * those are not needed in code coverage report.
+         */
+        'src/{benchmarks,tests}/**',
+      ],
       reporter: ['lcov', 'text'],
     },
   },
