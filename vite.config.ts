5be814e3713391d8a304c2ba229970be118cd3cf
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2023-02-19 23:53:37 +0800

    chore: update code comment
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
     coverage: {
       exclude: [
         /**
-         * NOTE: Code coverage always collect other files, e.g. `constants.ts` inside `tests` folder.
+         * NOTE: Code coverage collects other files inside `tests` folder, e.g. `constants.ts` but
+         * those are not needed in code coverage report.
          */
         'src/{benchmarks,tests}/**',
       ],
